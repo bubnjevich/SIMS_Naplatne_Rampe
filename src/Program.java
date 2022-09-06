a5314b18313fd77ff1e79d756a394ee964005386
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2022-09-06 10:29:56 +0200

    deleted unused lines

--- src/Program.java
+++ src/Program.java
@@ -28,27 +28,7 @@ public class Program {
 	public static void main(String[] args) {
 		LoginFrame login = new LoginFrame();
 		login.setVisible(true);
-//		TollStationRepository to = new TollStationRepository();
-//		TollStation tollstation = to.findTollStationByName("Station Novi Sad 1");
-//
-//		PassesRepository pas = new PassesRepository();
-//		ArrayList<Pass> passe = pas.getPasses();
-//
-//		PaymentFrame login = new PaymentFrame(passe.get(0),tollstation);
-//		login.setVisible(true);
-//		Address adress = new Address("DDASDASDASDAS");
-//		TagSeller user = new TagSeller("bane","des","des","des", Gender.MALE,"dsdasdas",adress, Role.TAGSELLER);
-//
-//		UserService userService = new UserService();
-//		userService.createUser(user);
-//		RoadSectionRepository rd = new RoadSectionRepository();
-//		for (RoadSection road:rd.getRoadSections()){
-//			System.out.println(road.getData());
-//		}
-//		PriceListRepository pr = new PriceListRepository();
-//		for (PriceListItem pra:pr.getPriceLists()){
-//			System.out.println(pra.getData());
-//		}
+
 
 
 
